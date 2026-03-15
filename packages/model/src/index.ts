bd83e055731f009eefa5f88389dc4f105df5653e
goxiaoy <xy1996mail@gmail.com>
2026-03-15 18:17:00 +0800

    feat(model): add model service types

--- packages/model/src/index.ts
+++ packages/model/src/index.ts
@@ -1,3 +1,15 @@
 export function createModelService() {
   throw new Error("Not implemented");
 }
+
+export type {
+  CreateModelServiceInput,
+  LocalEmbeddingExtractor,
+  LocalRerankerInputs,
+  LocalRerankerRuntime,
+  ModelDownloadStatus,
+  ModelDownloadStatusStore,
+  ModelDownloadTask,
+  ModelDownloadTaskState,
+  ModelService,
+} from "./model.service.types";
